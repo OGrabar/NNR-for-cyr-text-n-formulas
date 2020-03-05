c42a6952116dffc9b5353f24722ea4aebefd0c8c
EduardLimonov <49101914+EduardLimonov@users.noreply.github.com>
2020-03-05 20:18:44 +0300

    Add files via upload

--- NeurNet.py
+++ NeurNet.py
@@ -1,6 +1,17 @@
-from Block import *
+from __future__ import annotations
+import numpy as np
+from typing import *
 import saving
+import pybrain
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import ReluLayer
+from pybrain.structure import SigmoidLayer
+from pybrain.structure import TanhLayer
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
 
+
+RELU, SIGMOID, TANH = 'relu', 'sigmoid', 'tanh'
 
 def interpretRes(res: np.array):
     #vars = [i for i in range(10)]
@@ -9,39 +20,39 @@ def interpretRes(res: np.array):
     return list(res).index(t)
 
 
-
 class NeurNet:
-    first : Block
-    last : Block
-    ins : np.array
-    def __init__(self, first: Block, last: Block):
-        self.first = first
-        self.last = last
+    nn: pybrain.Network
+    ds: SupervisedDataSet
+    trainer: BackpropTrainer
 
-        self.ins = None # входы нейросети
+    def __init__(self, layers: Tuple[int,], func: Union[RELU, SIGMOID, TANH] =RELU):
+        if func == RELU:
+            hc = ReluLayer
+        elif func == SIGMOID:
+            hc = SigmoidLayer
+        elif func == TANH:
+            hc = TanhLayer
+        else:
+            raise Exception("Invalid func param")
+        self.nn = buildNetwork(*layers, hiddenclass=hc)
+        self.ds = SupervisedDataSet(layers[0], layers[-1])
+        self.trainer = BackpropTrainer(self.nn, self.ds)
 
-    def begin(self) -> np.array:
-        return self.last.getOuts(self.ins)
+    def signal(self, ins: np.ndarray) -> np.ndarray:
+        return self.nn.activate(ins)
 
-    def setIns(self, ins): # установить входы в нейросеть
-        if (len(ins) != self.first.nIns):
-            print(len(ins))
-            raise Exception("Длина столбца входов")
+    def addSample(self, ins: np.ndarray, outs: np.ndarray) -> None:
+        self.ds.addSample(ins, outs)
 
-        self.ins = ins
+    def train(self, epoch: int=1) -> float:
+        # возвращает значение квадратичной ошибки на последней эпохе
+        for i in range(epoch - 1):
+            self.trainer.train()
+        return self.trainer.train()
 
-    def getIns(self):
-        return self.ins
-
-    def improve(self, errors, learning_rate):
-        self.last.improve(errors, learning_rate)
-
-    def save(self, filename: str ='default'):
+    def save(self, filename: str = 'default'):
         saving.saveObj(self, filename + '.nn')
 
-    def load(self, filename: str ='default'):
-        nn = saving.loadObj(filename + '.nn')
-        self.first, self.last, self.ins = nn.first, nn.last, nn.ins
-
-    def stopRes(self):
-        pass
+    def load(self, filename: str = 'default'):
+        other = saving.loadObj(filename + '.nn')
+        self.nn, self.ds, self.trainer = other.nn, other.ds, other.trainer
